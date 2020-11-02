99ca26697a0d729859542d11c5ec7bb70c5292fc
davidvdlinden <d.m.vanderlinden@student.utwente.nl>
2020-11-02 12:33:18 +0100

    actualy turns on and off leds now

--- modecontrol.py
+++ modecontrol.py
@@ -18,6 +18,9 @@ class Modecontroler(object):
         # switch tings
         self.sw = pyb.Switch()
         self.sw.callback(self.run)
+        self.led1 = pyb.LED(1)
+        self.led2 = pyb.LED(2)
+        self.led3 = pyb.LED(3)
         return
 
     def run(self):  # does one iteration
@@ -28,10 +31,15 @@ class Modecontroler(object):
         # Entry action
 
         # Action
-        print('no leds are lighting up')  # todo actually off on the leds
+        # led thing
+        print('no leds are lighting up')
+        self.led1.off()
+        self.led2.off()
+        self.led3.off()
+        # non led thing
         self.globalstate = 0
 
-        # State guards (transitions) # todo update to include state 4
+        # State guards (transitions)
         if self.last_state == StatesIN.STATE1:
             self.last_state = StatesIN.STATE0
             self.state = StatesIN.STATE2
@@ -50,7 +58,12 @@ class Modecontroler(object):
 
     def state1(self):
         # Entry action
-        print('led 1 is on')  # todo actually turn on the led
+        # led thing
+        print('led 1 is on')
+        self.led1.on()
+        self.led2.off()
+        self.led3.off()
+        # non led thing
         self.globalstate = 1
         print('were calling the fuction state 1', 'and globalstate is:', self.globalstate)
 
@@ -70,7 +83,12 @@ class Modecontroler(object):
         # todo same as above state but there is a change to the reference
 
         # Action
-        print('led 2 is on')  # todo actually turn on the led
+        # led thing
+        print('led 2 is on')
+        self.led1.off()
+        self.led2.on()
+        self.led3.off()
+        # non led things
         self.globalstate = 2
 
         # State guards (transitions)
@@ -86,7 +104,12 @@ class Modecontroler(object):
             # todo run initialisation sequence
 
         # Action
-        print('led 3 is on')  # todo actually turn on the led
+        # led things
+        print('led 3 is on')
+        self.led1.off()
+        self.led2.off()
+        self.led3.on()
+        # non led things
         self.globalstate = 3
         # todo run set of pre-programed moments
 
--- statesIN.py
+++ statesIN.py
@@ -3,4 +3,3 @@ class StatesIN(object):
     STATE1 = 'state1' # non suportive
     STATE2 = 'state2' # suportive
     STATE3 = 'state3' # demmo mode
-    # todo add 4th state for pre-programed song playing
